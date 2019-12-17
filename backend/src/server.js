abaf3aee6edde5be57e6a0da539dc1d27e40df5c
Douglas Batista <douglasresendebatista@gmail.com>
2019-12-17 11:23:12 -0300

    Ajuste

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const http = require('http');
 const WebSocketServer = require('ws').Server;
 const port = process.env.PORT || 3300;
+const httpPort = process.env.HTTPPORT || 3301;
 
 require('./mongoose');
 
@@ -13,7 +14,7 @@ server.use(cors());
 server.use(express.static(__dirname + '/'));
 
 const httpServer = http.createServer(server);
-httpServer.listen(port);
+httpServer.listen(httpPort);
 
 var wss = new WebSocketServer({ server: httpServer });
 wss.on('connection', function(ws) {
@@ -21,9 +22,14 @@ wss.on('connection', function(ws) {
     ws.send(JSON.stringify(new Date()), function() {});
   }, 1000);
 
+  console.log('websocket connection open');
+
   ws.on('close', function() {
+    console.log('websocket connection close');
     clearInterval(id);
   });
 });
 
 require('./routes/apiRoutes')(server);
+
+server.listen(port, () => console.log(`Servidor ativo na porta ${port}`));
