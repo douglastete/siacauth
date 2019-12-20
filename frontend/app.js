d2e65221429e5282ef2fe3d273b41f7920cf2f35
Douglas Batista <douglasresendebatista@gmail.com>
2019-12-20 09:01:04 -0300

    Organizando código

--- frontend/app.js
+++ frontend/app.js
@@ -2,17 +2,24 @@ import AddToHomeScreen from 'a2hs.js';
 
 import { authFirebase } from './src/assets/js/auth';
 
-authFirebase.listenerState();
+init();
 
-const img = document.getElementById('imgA2HS');
-img.remove();
-img.removeAttribute('hidden');
+function init() {
+  authFirebase.listenerState();
+  adjustImagePWA();
+}
 
-AddToHomeScreen({
-  brandName: 'SIAC Auth',
-  backgroundColor: '#428bca',
-  color: 'white',
-  logoImage: img.outerHTML,
-  htmlContent:
-    'Instale o app <strong>SIAC Auth</strong> no seu dispositivo iOS: clique em compartilhar and <strong>Adicionar à Tela de Início</strong>'
-});
+function adjustImagePWA() {
+  const img = document.getElementById('imgA2HS');
+  img.remove();
+  img.removeAttribute('hidden');
+
+  AddToHomeScreen({
+    brandName: 'SIAC Auth',
+    backgroundColor: '#428bca',
+    color: 'white',
+    logoImage: img.outerHTML,
+    htmlContent:
+      'Instale o app <strong>SIAC Auth</strong> no seu dispositivo iOS: clique em compartilhar and <strong>Adicionar à Tela de Início</strong>'
+  });
+}
